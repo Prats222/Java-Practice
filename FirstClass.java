123dd2f891e29462e6d3241febc3cd02b40613d5
Prats222 <126672828+Prats222@users.noreply.github.com>
2023-10-18 12:59:02 +0530

    1

---
+++ FirstClass.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        int n = 4;
+        int m = 5;
+
+        // outer loop
+        for (int i = 1; i <= n; i++) {
+            // inner loop
+            for (int j = 1; j <= m; j++) {
+                if ((i == 2 || i == 3) && (j == 2 || j == 3 || j == 4)) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ rotate.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class rotate {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = n; j >= 1; j--) {
+                if (i >= j) {
+                    System.out.print(n);
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
