813b52476732333e50930f802c27f9e32c1a893f
Prats222 <126672828+Prats222@users.noreply.github.com>
2023-10-18 13:31:44 +0530

    2

---
+++ numP.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class numP {
+    public static void main(String[] args) {
+        // int n = 5;
+
+        // backwards inverted number
+        // for (int i = n; i >= 1; i--) {
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print(j + " ");
+        // }
+        // System.out.println();
+        // }
+
+        // floyds triangle
+        // int number = 1;
+        // for (int i = 1; i <= n; i++) {
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print(number + " ");
+        // number++;
+        // }
+        // System.out.println();
+        // }
+
+        // triange of 0/1(even/odd)
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                if ((i + j) % 2 == 0) {
+                    System.out.print("1 ");
+                } else {
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
