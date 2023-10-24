9138f7ddaf920c263c7665639e2e14241dbb86f2
Prats222 <126672828+Prats222@users.noreply.github.com>
2023-10-24 15:13:10 +0530

    Update functions.java

--- functions.java
+++ functions.java
@@ -1,19 +1,22 @@
 import java.util.*;
 
 public class functions {
-    public static void Calc(int a) {
-        int factorial = 1;
-        for (int i = a; i >= 1; i--) {
-            factorial *= i;
-        }
-        System.out.println(factorial);
-        return;
-    }
+    // public static void Calc(int a) {
+    // int factorial = 1;
+    // for (int i = a; i >= 1; i--) {
+    // factorial *= i;
+    // }
+    // System.out.println(factorial);
+    // return;
+    // }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
+        // int a = sc.nextInt();
 
-        Calc(a);
+        // Calc(a);
+        int r = sc.nextInt();
+        double circumference = 2 * r * 3.14;
+        System.out.println(circumference);
     }
 }
