978502e7a5190619bb5d067d2db91e727e1ad67d
Prats222 <126672828+Prats222@users.noreply.github.com>
2023-11-03 16:44:56 +0530

    Create Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Fibonacci {
+    public static void Fibbo(int n) {
+        int a = 0, b = 1, temp;
+        System.out.print(a + " " + b);
+        for (int i = 2; i < n; i++) {
+            temp = a + b;
+            System.out.print(" " + temp);
+            a = b;
+            b = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Fibbo(n);
+    }
+}
