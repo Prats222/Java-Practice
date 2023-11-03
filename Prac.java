c99f7e01739ef5ff663290e8ac750290460eb018
Prats222 <126672828+Prats222@users.noreply.github.com>
2023-11-03 16:26:57 +0530

    Update Prac.java

--- Prac.java
+++ Prac.java
@@ -1,11 +1,23 @@
 import java.util.*;
 
 public class Prac {
+    public static int GCD(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // System.out.println(Math.pow(a, b));
         int a = sc.nextInt();
         int b = sc.nextInt();
-
-        System.out.println(Math.pow(a, b));
+        int ans = GCD(a, b);
+        System.out.println(ans);
     }
 }
