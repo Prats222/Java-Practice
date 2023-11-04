6f16e873f21880954593428e3b66be03450afa23
Prats222 <126672828+Prats222@users.noreply.github.com>
2023-11-04 23:53:52 +0530

    Create Array.java

---
+++ Array.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Array {
+    public static void main(String[] args) {
+        int[] marks = new int[3];
+        marks[0] = 98;
+        marks[1] = 97;
+        marks[2] = 96;
+        System.out.println(marks[0]);
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println(marks[i]);
+        }
+    }
+}
