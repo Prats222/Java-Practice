8840b36206f196831043497f1a78a850300ac39d
Prats222 <126672828+Prats222@users.noreply.github.com>
2023-11-22 23:00:15 +0530

    .l

---
+++ Sbuilder.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class Sbuilder {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Prats");
+        System.out.println(sb);
+    }
+}
--- Strings.java
+++ Strings.java
@@ -24,5 +24,6 @@ public class Strings {
         String sentence = " My name is Prats";
         String name = sentence.substring(11, sentence.length());
         System.out.println(name);
+
     }
 }
