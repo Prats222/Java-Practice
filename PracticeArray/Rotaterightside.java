9f16688138cdd269268d184c0f3c6cf98ed6f005
Prats222 <126672828+Prats222@users.noreply.github.com>
2024-06-14 17:27:00 +0530

    hh

---
+++ PracticeArray/Rotaterightside.java
@@ -0,0 +1,38 @@
+package PracticeArray;
+
+import java.util.*;
+
+public class Rotaterightside {
+    public static void reverse(int[] arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void rotate(int[] arr, int k) {
+        int n = arr.length;
+        k = k % n;
+        reverse(arr, 0, n - k - 1);
+        reverse(arr, n - k, n - 1);
+        reverse(arr, 0, n - 1);
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int k = sc.nextInt();
+        rotate(arr, k);
+
+    }
+}
